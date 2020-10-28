a9270e8029195216498552fef4a3b69c3ebf3d24
Alec Fong <alecfong1@gmail.com>
2020-10-27 20:49:38 -0700

    making operation ids make more sense and specifying title

--- brev_rest/app.py
+++ brev_rest/app.py
@@ -1,3 +1,4 @@
+import pathlib
 from . import rest
 from . import asgi
 from . import route_loader
@@ -6,7 +7,8 @@ from . import route
 
 def run(*, app_path: str) -> None:
     routes = route_loader.make_routes(app_path=app_path)
-    app = asgi.BrevFastApi(routes)
+    title = get_default_title(app_path=app_path)
+    app = asgi.BrevFastApi(title, routes)
 
     _run(app)
 
@@ -17,9 +19,15 @@ def _run(rest_app: rest.App) -> None:
 
 def get_server(*, app_path: str):
     routes = route_loader.make_routes(app_path=app_path)
-    app = asgi.BrevFastApi(routes)
+    title = get_default_title(app_path=app_path)
+    app = asgi.BrevFastApi(title, routes)
 
     return _get_server(app)
+
+
+def get_default_title(*, app_path: str):
+    raw_parent = pathlib.Path(app_path).parent.stem
+    return raw_parent.replace("-", " ").replace("_", " ").capitalize()
 
 
 def _get_server(rest_app: rest.App):
--- brev_rest/asgi.py
+++ brev_rest/asgi.py
@@ -2,8 +2,10 @@ import typing
 import abc
 from starlette.types import ASGIApp
 import fastapi
+import fastapi.utils
+import re
 import uvicorn  # type: ignore
-
+from starlette.routing import get_name
 
 from . import rest
 from . import route as brev_route
@@ -16,8 +18,8 @@ class Asgi(rest.App):
 
 
 class BrevFastApi(Asgi):
-    def __init__(self, routers: typing.List[brev_route.Router]):
-        self.app = fastapi.FastAPI()
+    def __init__(self, title, routers: typing.List[brev_route.Router]):
+        self.app = fastapi.FastAPI(title=title)
 
         for r in routers:
             self.add_router(r)
@@ -25,12 +27,40 @@ class BrevFastApi(Asgi):
     def add_router(self, router: brev_route.Router):
         fastapi_api_router = fastapi.APIRouter()
         for r in router.routes:
-            fastapi_api_router.add_api_route(
-                r.path, r.endpoint, methods=[r.endpoint.__name__], *r.args, **r.kwargs
+            method = get_name(r.endpoint)
+            operation_id = self.generate_operation_id_for_path(
+                name=router.path, path=r.path, method=method
             )
+            fastapi_api_router.add_api_route(
+                r.path,
+                r.endpoint,
+                methods=[method],
+                operation_id=operation_id,
+                **r.kwargs
+            )
+
         self.app.include_router(
             fastapi_api_router, prefix=router.path, tags=[router.name]
         )
+
+    def generate_operation_id_for_path(
+        self, *, name: str, path: str, method: str
+    ) -> str:
+        """
+        Make names like get_xyz, post_xyz_subpath
+        TODO: Improve by using variable subpaths and return schema todo things like get_all_endpoints or get_enpoint_by_id
+        """
+        name = self.clean_name_str(name)
+        path = self.clean_name_str(path)
+        join = "_" if len(path) > 0 else ""
+        operation_id = name + join + path
+        operation_id = method.lower() + "_" + operation_id
+        return operation_id
+
+    def clean_name_str(self, value: str) -> str:
+        value = re.sub("[//{{}}]", "", value)
+        value = re.sub("[^0-9a-zA-Z_]", "_", value)
+        return value
 
     def get_server(self) -> ASGIApp:
         return self.app
